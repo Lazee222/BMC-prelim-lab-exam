66e58701a1d3bf4be306f261a2b3f7165a68d6ab
Lazee222 <lanceterrazola8@gmail.com>
2026-08-13 12:23:31 +0800

    prelim lab exam Student Performance  Tracker

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("ENTER YOUR NAME: ");
+  String? name = stdin.readLineSync();
+
+  stdout.write("ENTER YOUR AGE: ");
+  int? age = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Quiz 1: ");
+  int? quiz = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Quiz 2: ");
+  int? quiz2 = int.parse(stdin.readLineSync()!);
+  
+  stdout.write("Quiz 3: ");
+  int? quiz3 = int.parse(stdin.readLineSync()!);
+
+  
+  double totalAve = ((quiz + quiz2 + quiz3)/3);
+
+  print("Average: $totalAve");
+  if (totalAve >=75) {
+    print("Status: Passed");
+    print("Remark: Good job, keep it up! ");
+  }  else {
+    print("Status: Failed");
+    print("Remark: Better Luck Next Time");
+  }
+  }
